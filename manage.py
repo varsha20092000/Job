f6447a68db3cadd3676b4b205515baee5aeb6fa8
Varsha <varsha20092000@gmail.com>
2025-07-11 12:16:55 +0530

    Updated manage.py and added full_data.json for initial data

--- manage.py
+++ manage.py
@@ -4,18 +4,23 @@ import os
 import sys
 
 
+import os
+from django.core.management import call_command
+
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Jobs.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+
+    # Add this block to load data during deployment if not already loaded
+    if os.environ.get("RENDER") == "true":
+        try:
+            call_command("loaddata", "App/jobs.json")
+            print("Jobs data loaded successfully.")
+        except Exception as e:
+            print("Error loading jobs data:", e)
+
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
+
 
 
 if __name__ == '__main__':
