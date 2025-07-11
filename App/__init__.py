05ce5ab3cc718c233af948d5cf5b95069b09489e
Varsha <varsha20092000@gmail.com>
2025-07-11 18:47:17 +0530

    Fix: Load full_data.json after apps ready on Render

--- App/__init__.py
+++ App/__init__.py
@@ -1,1 +1,2 @@
-default_app_config = 'App.signals.AppReady'
+default_app_config = 'App.apps.AppConfig'
+
--- App/apps.py
+++ App/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+import os
 
 class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'App'
+
+    def ready(self):
+        if os.environ.get("RENDER") == "true":
+            from django.core.management import call_command
+            from django.db.utils import OperationalError
+            try:
+                call_command("loaddata", "full_data.json")
+                print("✅ Loaded full_data.json successfully on Render.")
+            except OperationalError:
+                print("⚠️ Database not ready yet.")
+            except Exception as e:
+                print(f"❌ Error loading fixture: {e}")
