ce34da18c7d5800b73ecdbad23dde572d51d5f9d
Varsha <varsha20092000@gmail.com>
2025-07-14 16:18:14 +0530

    Fixed app import and ensured lowercase app name

--- app/forms.py
+++ app/forms.py
@@ -48,7 +48,7 @@ class CompanyForm(forms.ModelForm):
             'logo'
         ]
 
-from .models import JobApplication
+from app.models import JobApplication
 class JobApplicationForm(forms.ModelForm):
     class Meta:
         model = JobApplication
