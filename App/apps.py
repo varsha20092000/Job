e15dbb0d9e1fa131e22b8e6204249e9789ac1810
Varsha <varsha20092000@gmail.com>
2025-07-26 22:57:07 +0530

    Add  templates

---
+++ App/apps.py
@@ -0,0 +1,15 @@
+from django.apps import AppConfig
+from django.core.management import call_command
+import os
+
+class JobsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'App'
+
+    def ready(self):
+        if os.environ.get("RENDER") == "true":
+            try:
+                call_command("loaddata", "admin_jobs_fixed.json")
+                call_command("loaddata", "applications_from_sqlite_fixed.json")
+            except Exception as e:
+                print(f"❌ Fixture load error: {e}")
